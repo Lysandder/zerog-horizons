0b78f53006467d1928cddb176bac0dcfb852c764
Lysandder <eketcum61@gmail.com>
2025-10-05 23:29:48 +0500

    20 variables

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
 public class Main {
   public static void main(String[] args) {
-    
+    String companyName = "ZeroG Horizons";
+    int foundingYear = 1991;
+
+    // Address of headquarters
+    String country = "Uzbekistan";
+    String city = "Tashkent";
+
+    int activeSpacecrafts = 12;
+
+    int trainingHours = 200;
+    double trainingSuccessRate = 98.5d;
+
+    int designEngineers = 45;
+    int simulationSpecialists = 20;
+    int propulsionEngineers = 15;
+    int aerodynamicsEngineers = 18;
+    int prototypeTestsPerYear = 40;
+
+    int customersServed = 350;
+    double ticketPrice = 2.5d; // thousands usd
+    int orbitalHotelRooms = 20;
+    int orbitalHotelCapacity = 50;
+    double customerSatisfactionAvg = 96.5d;
+    int privateFlightsBooked = 40;
+    double revenue = 87.5d; // millions usd
+    int waitingClients = 1200;
   }
 }
