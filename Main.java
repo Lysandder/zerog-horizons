9cba240ec2f8a66e496498e22d05e05be91b692e
Lysandder <eketcum61@gmail.com>
2025-10-12 19:54:29 +0500

    More variables and finals

--- Main.java
+++ Main.java
@@ -7,10 +7,12 @@ public class Main {
     final String country = "Uzbekistan";
     final String city = "Tashkent";
 
-    byte activeSpacecrafts = 12;
-
-    short trainingHours = 200;
+    final short trainingHours = 200;
     double trainingSuccessRate = 98.5d;
+
+    byte activeSpacecrafts = 12;
+    double maxSpeed = 7.9; // km per second
+    byte luxuryCapsuleSeats = 8;
 
     byte designEngineers = 45;
     byte simulationSpecialists = 20;
@@ -26,5 +28,11 @@ public class Main {
     byte privateFlightsBooked = 40;
     double revenue = 262.5d; // millions usd
     short waitingClients = 1200;
+    
+    byte governmentContracts = 8;
+    byte globalInvestors = 150;
+
+    short totalEmployees = 3200;
+    final boolean remoteWorkAllowed = false;
   }
 }
